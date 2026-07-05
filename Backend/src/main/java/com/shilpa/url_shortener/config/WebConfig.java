a2f3294863ca7517fc9cd362c42db1905ea11d3c
ShilpaReddy2006 <shilpareddy24871@gmail.com>
2026-07-05 22:48:20 +0530

    Update WebConfig.java

--- Backend/src/main/java/com/shilpa/url_shortener/config/WebConfig.java
+++ Backend/src/main/java/com/shilpa/url_shortener/config/WebConfig.java
@@ -18,7 +18,8 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5500",
-                                "http://127.0.0.1:5500"
+                                "http://127.0.0.1:5500",
+                                "https://url-shortener-q8hc.onrender.com"
                         )
                         .allowedMethods("*")
                         .allowedHeaders("*");
